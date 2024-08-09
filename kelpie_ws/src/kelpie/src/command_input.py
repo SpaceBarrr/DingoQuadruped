- Fixed joystick input scaling. - Added in the ability to apply a transfer fucntion to joystick input.

kelpie_ws/src/kelpie/src/command_input.py
@@ -57,8 +57,11 @@ class InputSubscriber:
         # self.developing_command.joystick_control_event = self.joystick_control_event
 
         # Continuous Commands
-        x_vel = command.y * self.config.max_x_velocity  # ly
-        y_vel = command.x * self.config.max_y_velocity  # lx
+        x_vel = min(self.config.x_vel_tf(command.y),  self.config.max_x_velocity)   # ly
+        y_vel = min(self.config.y_vel_tf(command.x),  self.config.max_y_velocity)   # lx
+
+        # x_vel = command.y * self.config.max_x_velocity  # ly
+        # y_vel = command.x * self.config.max_y_velocity  # lx
         self.developing_command.horizontal_velocity = np.round(np.array([x_vel, y_vel]), self.rounding_dp)
 
         # Attitude

kelpie_ws/src/kelpie/src/main_driver.py
@@ -127,7 +127,8 @@ class KelpieDriver:
                     self.imu.read_orientation() if self.use_imu else np.array([0, 0, 0])
                 )
                 [yaw, pitch, roll] = self.state.euler_orientation
-                # print('Yaw: ',np.round(yaw,2),'Pitch: ',np.round(pitch,2),'Roll: ',np.round(roll,2))
+                # print('Yaw: ',np.round(yaw,2),'Pitc
+                # ]\h: ',np.round(pitch,2),'Roll: ',np.round(roll,2))
                 # Step the controller forward by dt
                 self.controller.run(self.state, command)
 