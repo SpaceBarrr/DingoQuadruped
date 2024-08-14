4318dd3d8bc765a4a89eadf2d84e149e2a2fe49b
Will <wlii0044@student.monash.edu>
2024-08-15 00:53:47 +1000

    - Fixed calibrate_servo.py initial offset values.

--- kelpie_ws/src/kelpie_hardware_interface/scripts/calibrate_servo.py
+++ kelpie_ws/src/kelpie_hardware_interface/scripts/calibrate_servo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 import yaml
-import os 
+import os
 import numpy as np
 
 from kelpie_hardware_interface.servo.Interface import ServoInterface
@@ -9,7 +9,8 @@ from kelpie_common.Config import Leg_linkage, Configuration
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-class CalibrateServo():
+
+class CalibrateServo:
     ''' 
     0  [[front_right_hip, front_left_hip, back_right_hip, back_left_hip]
     1  [front_right_upper, front_left_upper, back_right_upper, back_left_upper]
@@ -20,37 +21,40 @@ class CalibrateServo():
         self.pwm_max = 2400
         self.pwm_min = 370
         self.servo_interface = ServoInterface(Leg_linkage(Configuration()))
-        self.servo_interface.physical_calibration_offsets = np.zeros((3,4))
+        self.servo_interface.physical_calibration_offsets = np.zeros((3, 4))
         self.servo_angles = np.zeros((3, 4))
-        self.ANGLES = {"fr r":0, "fr u":0, "fr l":90, "fl r":0, "fl u":0, "fl l":90, "rr r":0, "rr u":0, "rr l":90, "rl r":0, "rl u":0, "rl l":90}
+        self.ANGLES = {"fr r": 0, "fr u": 0, "fr l": 90, "fl r": 0, "fl u": 0, "fl l": 90, "rr r": 0, "rr u": 0,
+                       "rr l": 90, "rl r": 0, "rl u": 0, "rl l": 90}
         if os.path.isfile(f"{DIR_PATH}/calibrate_servo_angles.yaml"):
             with open(f"{DIR_PATH}/calibrate_servo_angles.yaml") as stream:
-                raw_angles = yaml.load(stream,Loader=yaml.SafeLoader)
+                raw_angles = yaml.load(stream, Loader=yaml.SafeLoader)
             self.offset = {
-                "fr r": raw_angles["fr"]["roll"],"fr u": raw_angles["fr"]["upper"],"fr l": raw_angles["fr"]["lower"],
-                "fl r": raw_angles["fl"]["roll"],"fl u": raw_angles["fl"]["upper"],"fl l": raw_angles["fl"]["lower"],
-                "rr r": raw_angles["rr"]["roll"],"rr u": raw_angles["rr"]["upper"],"rr l": raw_angles["rr"]["lower"],
-                "rl r": raw_angles["rl"]["roll"],"rl u": raw_angles["rl"]["upper"],"rl l": raw_angles["rl"]["lower"]
+                "fr r": raw_angles["fr"]["roll"], "fr u": raw_angles["fr"]["upper"], "fr l": raw_angles["fr"]["lower"],
+                "fl r": raw_angles["fl"]["roll"], "fl u": raw_angles["fl"]["upper"], "fl l": raw_angles["fl"]["lower"],
+                "rr r": raw_angles["rr"]["roll"], "rr u": raw_angles["rr"]["upper"], "rr l": raw_angles["rr"]["lower"],
+                "rl r": raw_angles["rl"]["roll"], "rl u": raw_angles["rl"]["upper"], "rl l": raw_angles["rl"]["lower"]
             }
         else:
             print("Could not find calibrate_servo_angles.yaml - Using defaults...")
-            self.offset = {"fr r":0, "fr u":0, "fr l":90, "fl r":0, "fl u":0, "fl l":90, "rr r":0, "rr u":0, "rr l":90, "rl r":0, "rl u":0, "rl l":90}
+            self.offset = {"fr r": 0, "fr u": 0, "fr l": 0, "fl r": 0, "fl u": 0, "fl l": 0, "rr r": 0, "rr u": 0,
+                           "rr l": 0, "rl r": 0, "rl u": 0, "rl l": 0}
         self._set_start_pos()
 
     def _set_start_pos(self):
         self.servo_interface.physical_calibration_offsets = self._format_offset()
-        self.servo_angles = np.array([[self.ANGLES["fr r"], self.ANGLES["fl r"], self.ANGLES["rr r"], self.ANGLES["rl r"]],
-                        [self.ANGLES["fr u"], self.ANGLES["fl u"], self.ANGLES["rr u"], self.ANGLES["rl u"]],
-                        [self.ANGLES["fr l"], self.ANGLES["fl l"], self.ANGLES["rr l"], self.ANGLES["rl l"]]])
+        self.servo_angles = np.array(
+            [[self.ANGLES["fr r"], self.ANGLES["fl r"], self.ANGLES["rr r"], self.ANGLES["rl r"]],
+             [self.ANGLES["fr u"], self.ANGLES["fl u"], self.ANGLES["rr u"], self.ANGLES["rl u"]],
+             [self.ANGLES["fr l"], self.ANGLES["fl l"], self.ANGLES["rr l"], self.ANGLES["rl l"]]])
 
         self.servo_interface.set_servo_angles(self.servo_angles)
 
     def _format_offset(self):
         return np.array([
-            [self.offset["fr r"],  self.offset["fl r"],  self.offset["rr r"],  self.offset["rl r"]],
-            [ self.offset["fr u"],  self.offset["fl u"],  self.offset["rr u"],  self.offset["rl u"]],
-            [ self.offset["fr l"],  self.offset["fl l"],  self.offset["rr l"],  self.offset["rl l"]]
-            ])
+            [self.offset["fr r"], self.offset["fl r"], self.offset["rr r"], self.offset["rl r"]],
+            [self.offset["fr u"], self.offset["fl u"], self.offset["rr u"], self.offset["rl u"]],
+            [self.offset["fr l"], self.offset["fl l"], self.offset["rr l"], self.offset["rl l"]]
+        ])
 
     def run(self):
         motor = "fr r"
@@ -73,7 +77,7 @@ class CalibrateServo():
                     break
                 try:
                     self.offset[motor] = float(value)
-                    self.servo_interface.physical_calibration_offsets =  self._format_offset()
+                    self.servo_interface.physical_calibration_offsets = self._format_offset()
                     self.servo_interface.set_servo_angles(self.servo_angles)
                 except ValueError as error:
                     print(f"\tInvalid angle: {error}")
@@ -85,6 +89,7 @@ class CalibrateServo():
                       "rl": {"roll": self.offset["rl r"], "upper": self.offset["rl u"], "lower": self.offset["rl l"]}
                       }
         yaml.dump(new_angles, open(f"{DIR_PATH}/calibrate_servo_angles.yaml", "w+"))
+
 
 if __name__ == "__main__":
     calibrate_ob = CalibrateServo()
