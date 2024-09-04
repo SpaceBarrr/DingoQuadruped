603325acefc489f50aaaffd0f04fb8d0dd8f4a85
Cian Rodriguez <crod0007@student.monash.edu>
2024-09-05 09:46:33 +1000

    restructure imu folders

---
+++ kelpie_ws/src/kelpie_hardware_interface/scripts/LSM6DSV16X_imu_test.py
@@ -0,0 +1,14 @@
+from ..src.kelpie_hardware_interface.imu.Adafruit_CircuitPython_LSM6DS.adafruit_lsm6ds.lsm6dsv16x import LSM6DSV16X
+
+import time
+import board
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+sensor = LSM6DSV16X(i2c)
+
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+    print("")
+    time.sleep(0.5)
