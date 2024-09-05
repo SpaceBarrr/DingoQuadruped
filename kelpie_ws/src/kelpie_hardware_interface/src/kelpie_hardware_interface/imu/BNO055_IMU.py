45a8ca23422dcf23e36352225bd4fd6011f04dd9
Cian Rodriguez <crod0007@student.monash.edu>
2024-09-05 10:00:34 +1000

    update submodule commit

--- kelpie_ws/src/kelpie_hardware_interface/src/kelpie_hardware_interface/imu/BNO055_IMU.py
+++ kelpie_ws/src/kelpie_hardware_interface/src/kelpie_hardware_interface/imu/BNO055_IMU.py
@@ -30,7 +30,7 @@ class IMU:
             [yaw, pitch, roll] = self.sensor.euler
             yaw = m.radians(360 - yaw)
             pitch = m.radians(-pitch)
-            roll = m.radians(roll - 30)  # fixed offset to account for the IMU being off by 30 degrees
+            roll = m.radians(roll - 30)  # fixed offset to account for the IMU being off by 30 degrees in Dingo V1
             self.last_euler = [yaw, pitch, roll]
         except:
             self.last_euler = np.array([0, 0, 0])
