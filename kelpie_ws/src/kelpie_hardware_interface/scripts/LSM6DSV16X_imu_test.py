2dd406add84184f032347fc6c037332f6ce5d6ef
Cian odriguez <crod0007@student.monash.edu>
2024-09-05 15:27:02 +0000

    udpate sensor fusion in module

--- kelpie_ws/src/kelpie_hardware_interface/scripts/LSM6DSV16X_imu_test.py
+++ kelpie_ws/src/kelpie_hardware_interface/scripts/LSM6DSV16X_imu_test.py
@@ -1,7 +1,12 @@
-from ..src.kelpie_hardware_interface.imu.Adafruit_CircuitPython_LSM6DS.adafruit_lsm6ds.lsm6dsv16x import LSM6DSV16X
-
+import sys
+import os
 import time
 import board
+
+sys.path.insert(0, 
+                os.path.dirname(os.path.realpath(__file__)).replace('scripts','src') + \
+                "/kelpie_hardware_interface/imu/Adafruit_CircuitPython_LSM6DS/adafruit_lsm6ds")
+from lsm6dsv16x import LSM6DSV16X
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
@@ -10,5 +15,7 @@ sensor = LSM6DSV16X(i2c)
 while True:
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+    print(f"Temp: {sensor.temperature}")
+    print(f"Quaternion: {sensor.rot_vector}")
     print("")
     time.sleep(0.5)
