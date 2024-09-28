796e7c3a2bd2f26095e827cc24787e11bdb3d79b
Cian Rodriguez <crod0007@student.monash.edu>
2024-09-28 16:37:56 +1000

    add ros status

--- web_gui_normal/control.js
+++ web_gui_normal/control.js
@@ -1,22 +1,3 @@
-// Connecting to ROS
-// -----------------
-var ros = new ROSLIB.Ros({
-    url: 'ws://localhost:9090'
-});
-
-ros.on('connection', function () {
-    console.log('Connected to websocket server.');
-});
-
-ros.on('error', function (error) {
-    console.log('Error connecting to websocket server: ', error);
-});
-
-ros.on('close', function () {
-    console.log('Connection to websocket server closed.');
-});
-
-
 // Publishing to Topic for Stance Control
 // ------------------
 var command_input = new ROSLIB.Topic({
