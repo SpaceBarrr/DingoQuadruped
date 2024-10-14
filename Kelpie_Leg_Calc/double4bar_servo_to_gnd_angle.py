207111ecd69848c4dbd808970ceb4d9b07a4c0d4
Will <wlii0044@student.monash.edu>
2024-10-14 18:32:06 +1100

    -Renamed vec3d_float32.msg to vec_xyz_float32.msg due to ROS bug?

--- Kelpie_Leg_Calc/double4bar_servo_to_gnd_angle.py
+++ Kelpie_Leg_Calc/double4bar_servo_to_gnd_angle.py
@@ -2,12 +2,12 @@ from math import pi, atan2, acos, cos, sin, sqrt
 from numpy import deg2rad, rad2deg
 # Taken from: https://ocw.metu.edu.tr/pluginfile.php/3961/mod_resource/content/12/ch3/3-6.htm
 # Should be able to follow the same principle to reverse the calculation.
-# Constant lengths of first and second linkage. Using dictionaries to improve code readability as it is mathematical
+# Constant lengths of first and second linkage. Using dictionaries to improve code readability as it is mathematical``
 # equations.
-link1 = {"a1": 35, "a2": 35, "a3": 37.6, "a4": 43}
-link2 = {"a1": 130, "a2": 43, "a3": 130, "a4": 37}
+link1 = {"a1": 35, "a2": 35, "a3": 37.6307, "a4": 40}
+link2 = {"a1": 130, "a2": 40, "a3": 130, "a4": 50}
 
-servo_lower = 0    # Servo control for lower leg
+servo_lower = 20  # Servo control for lower leg
 servo_upper = 0    # Servo control for upper leg
 
 # Convert physical 0 deg origin to mathematical 0 deg origin by doing 180 - (54.7681 + servo_lower, 54.7681 is taken
@@ -26,8 +26,8 @@ theta2 = theta4 - 102.541 + 54.776 + servo_upper
 s1 = (link2["a2"] ** 2 + link2["a1"] ** 2 - 2 * link2["a2"] * link2["a1"] * cos(deg2rad(theta2))) ** (1 / 2)
 phi1 = pi - acos((link2["a1"] ** 2 + s1 ** 2 - link2["a2"] ** 2) / (2 * link2["a1"] * s1))
 psi1 = acos((link2["a4"] ** 2 + s1 ** 2 - link2["a3"] ** 2) / (2 * link2["a4"] * s1))
-
 theta_lower_leg_to_upper = rad2deg(phi1 - psi1)
+
 print(theta4)
 print(theta2)
 
---
+++ kelpie_ws/src/kelpie_logger/src/main.py
@@ -0,0 +1,32 @@
+import csv
+import os
+import time
+import rospy
+from kelpie.msg import imu
+import numpy as np
+
+FP = __file__.replace("/src/main.py", "/logs/")
+FILE_NAME = FP + time.strftime("%Y%m%d-%H%M%S") + ".csv"
+
+att = np.zeros((4,))
+acc = np.zeros((4,))
+gyro = np.zeros((4,))
+REF_TIME = time.time()
+def IMU_callback(imu_data: imu) -> None:
+    """
+    Callback function for IMU subscriber.
+    :param imu_data: The IMU data, expected as imu message format
+    :return: None
+    """
+    # Keep pointer/reference. Do not overwrite with new class reference.
+    att[:] = time.time() - REF_TIME ,imu_data.att.roll, imu_data.att.pitch, imu_data.att.yaw
+    acc[:] = time.time() - REF_TIME ,imu_data.acc.x, imu_data.acc.y, imu_data.acc.z
+    gyro[:] = time.time() - REF_TIME ,imu_data.gyro.x, imu_data.gyro.y, imu_data.gyro.z
+
+IMU = rospy.Subscriber("/kelpie/imu", imu, IMU_callback, queue_size=1)
+if not os.path.exists(FP):
+    os.makedirs(FP)
+
+with open(FILE_NAME,mode="w+", newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|')
+    spamwriter.writerow(att)
